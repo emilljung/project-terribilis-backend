866df7fe7124f691856bfd354b130edc5c6bf2ec
Emil Ljung <emil.ljung@cgi.com>
2023-09-19 11:09:29 +0200

    Setting up postgres, jpa, spring boot, hibernate & gradle

---
+++ src/main/java/com/project/terribilis/backend/ProjectTerribilisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.terribilis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTerribilisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTerribilisBackendApplication.class, args);
+	}
+
+}
